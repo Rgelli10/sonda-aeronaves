bb1dcefe8e75727e6cfbbbe712c49e8514c81b90
rgelli <rgelli1011@gmail.com>
2022-10-13 18:01:14 -0300

    teste

---
+++ aeronaves/src/main/java/br/com/sonda/aeronaves/AeronavesApplication.java
@@ -0,0 +1,13 @@
+package br.com.sonda.aeronaves;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AeronavesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AeronavesApplication.class, args);
+	}
+
+}
